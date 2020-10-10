def9b0e00c6bc64ffd3ab68f4f6d57e7faf6e03b
Costas <ceconomoujr@gmail.com>
2020-10-10 21:23:45 +0300

    Updates

---
+++ precache-manifest.ec1e9de98a0dd358a602540547b0dfc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777d1ded126aca940182d6604e90ed71",
+    "url": "/github.io/website/index.html"
+  },
+  {
+    "revision": "807d5f1fc68ad70ab39d",
+    "url": "/github.io/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/github.io/website/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/website/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807d5f1fc68ad70ab39d",
+    "url": "/github.io/website/static/js/main.e4ac6c2a.chunk.js"
+  },
+  {
+    "revision": "92c536eac1031345813f",
+    "url": "/github.io/website/static/js/runtime-main.cd1fb1da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
